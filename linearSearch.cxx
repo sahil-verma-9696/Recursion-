763d5128fc35ee74d5c3adbabe81dfffdd7cb7ee
Sahil verma <129593012+sahil-verma-9696@users.noreply.github.com>
2023-05-31 07:47:57 +0530

    Add file via phone
    Linear search from recursion

---
+++ linearSearch.cxx
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int linearSearch(int arr[], int key, int n){
+	
+	//base case 
+	if(n == 0 && key != arr[n-1])
+	return -1;
+	if(n == 0 )
+	return arr[0];
+	
+	//process
+	printf("n = %d\n",n);
+	if(key == arr[n-1]){
+		return n;
+	}
+	n--;
+	
+	//recursive statement
+	return linearSearch(arr,key,n);
+}
+void main(){
+	int arr[] = {4,2,3};
+	int n  = sizeof(arr)/sizeof(int);
+	printf("size = %d\n",n);
+	int key = 5;
+	printf("\npostion of key is = %d",linearSearch(arr,key,n));
+}
